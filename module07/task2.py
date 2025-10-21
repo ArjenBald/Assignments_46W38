d4e33e2a1abd94c0186a0e7eb4576605f6001815
Aleksei Subachev <alexei.subachev@gmail.com>
2025-10-21 16:09:08 +0200

    Add Part 2

--- module07/task2.py
+++ module07/task2.py
@@ -43,3 +43,20 @@ plt.show()
 # Create an interpolation function
 # We use 'extrapolate' to handle wind speeds outside the given range
 pitch_interpolator = interp1d(pitch_curve_wind_speed, blade_pitch, kind='linear', bounds_error=False, fill_value=(min(blade_pitch), max(blade_pitch)))
+
+
+# --- Task 2, Part 2: Determine and plot the time series of blade pitch ---
+
+# Use the interpolation function on the filtered wind speed data
+expected_pitch_variation = pitch_interpolator(filtered_wind_speed)
+
+# Plot the expected time series of the blade pitch
+plt.figure(figsize=(14, 7))
+plt.plot(time, expected_pitch_variation, label='Expected Blade Pitch')
+plt.title('Expected Time Series Variation of Blade Pitch')
+plt.xlabel('Time (s)')
+plt.ylabel('Blade Pitch (deg)')
+plt.grid(True)
+plt.legend()
+plt.savefig('expected_pitch_timeseries.png')
+plt.show()
